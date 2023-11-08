ec2748344331ecdb3eca0b538b618dd6524131a4
patrikvalentiny <patrikvalentiny@gmail.com>
2023-11-08 11:49:14 +0100

    update basic case test

--- apitests/UnitTest1.cs
+++ apitests/UnitTest1.cs
@@ -1,11 +1,33 @@
+using System.Net;
+
 namespace apitests;
 
 public class Tests
 {
 
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
-        Assert.Pass();
+        var _httpClient = new HttpClient();
+        
+        var url = "http://localhost:5000/api/v1/login";
+        
+        HttpResponseMessage response;
+        try
+        {
+            response = await _httpClient.GetAsync(url);
+            TestContext.WriteLine("THE FULL BODY RESPONSE: " + await response.Content.ReadAsStringAsync());
+        }
+
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+
+        using (new AssertionScope())
+        {
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
     }
 }
