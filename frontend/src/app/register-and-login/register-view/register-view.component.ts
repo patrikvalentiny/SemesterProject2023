9caca7098906f027dc506cc44f7c2e9c8a9e3a37
patrikvalentiny <patrikvalentiny@gmail.com>
2023-11-24 08:43:47 +0100

    update validators for login and register

--- frontend/src/app/register-and-login/register-view/register-view.component.ts
+++ frontend/src/app/register-and-login/register-view/register-view.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {AccountService} from "../account.service";
+import {HotToastService} from "@ngneat/hot-toast";
 
 @Component({
     selector: 'app-register-view',
@@ -11,10 +12,11 @@ export class RegisterViewComponent implements OnInit {
     passwordMatching: boolean = true;
     private readonly accountService: AccountService = inject(AccountService);
     private readonly fb: FormBuilder = inject(FormBuilder);
+    private readonly toastService = inject(HotToastService);
     registerForm = this.fb.group({
         username: [null, Validators.required],
-        password: [null, Validators.required],
-        confirmPassword: [null, Validators.required],
+        password: [null, Validators.required, Validators.minLength(4)],
+        confirmPassword: [null, Validators.required, Validators.minLength(4)],
         email: [null, Validators.email],
         firstName: [null],
         lastName: [null],
@@ -24,6 +26,7 @@ export class RegisterViewComponent implements OnInit {
         if (this.registerForm.value.confirmPassword !== this.registerForm.value.password) {
             this.passwordMatching = false;
             this.registerForm.controls.confirmPassword.reset();
+            this.toastService.error("Passwords do not match");
             return
         }
 
