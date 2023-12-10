6c580cf47d21417060d957aed724a950f7cb2d2e
patrikvalentiny <patrikvalentiny@gmail.com>
2023-12-10 10:45:30 +0100

    start with csv

---
+++ frontend/src/app/pages/csv-controls/csv-controls.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-csv-controls',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './csv-controls.component.html',
+  styleUrl: './csv-controls.component.css'
+})
+export class CsvControlsComponent {
+
+}
