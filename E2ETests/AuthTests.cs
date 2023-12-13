d3045bd3a7896e60dae2728f5f7c4f7f8e478578
patrikvalentiny <patrikvalentiny@gmail.com>
2023-12-13 07:52:49 +0100

    add registeronboardlogout test and register check profile values test

---
+++ E2ETests/AuthTests.cs
@@ -0,0 +1,28 @@
+﻿using apitests;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E2ETests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class AuthTests : PageTest
+{
+    [SetUp]
+    public async Task SetUp()
+    {
+        await Page.GotoAsync("http://localhost:4200");
+        Helper.TriggerRebuild();
+        await Helper.InsertUser1();
+        
+    }
+
+    [Test]
+    public async Task TestNotLoggedInUserCantAccessHome()
+    {
+        await Expect(Page).ToHaveURLAsync(new Regex(".*login"));
+
+        await Page.GotoAsync("http://localhost:4200/home");
+
+        await Expect(Page).ToHaveURLAsync("http://localhost:4200/login");
+    }
+}
